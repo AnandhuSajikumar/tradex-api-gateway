f83725eedfd46434180aa29cfc18ea0dff93180c
anandhu <anandhuzz916@gmail.com>
2026-02-23 12:57:52 +0530

    initial commit

---
+++ src/main/java/com/spring/tradexapigateway/TradexApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.spring.tradexapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradexApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradexApiGatewayApplication.class, args);
+    }
+
+}
